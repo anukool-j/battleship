b97d1890bdba5eaa0a92263a5c2e5ac344391268
Anukool Junnarkar <anukool.j@gmail.com>
2022-03-29 14:29:00 -0700

    Start work: push, maintaining exactly 1 connection.
    * How to wrap comments in vim ('gq<motion>')
    * Helpful debug message if the PORT is not defined

--- app.ts
+++ app.ts
@@ -16,8 +16,16 @@ class Board {
   }
 }
 
+// TODO: build client-side polling for push functionality. This will also be
+// used to ensure that there's only one connection for a <game,player> pair at
+// a time.
+
+// NOTE: wrap comments with 'gq<motion>'
 
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Battleship Classic listening on port ${port}`); 
+  if (port === undefined) {
+    console.log(`set -x PORT 3001`);
+  }
 });
