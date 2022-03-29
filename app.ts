692967a804e447274bf042f21eb83c4191bef81c
Anukool Junnarkar <anukool.j@gmail.com>
2022-03-29 14:59:25 -0700

    [break] time for zozo

--- app.ts
+++ app.ts
@@ -12,7 +12,12 @@ app.use(express.static('static'));
 // Battleship Classic
 
 class Board {
-  render(player: Player) {
+  render(
+    // 'red' and 'blue' information is all stored in the same Board, but
+    // 'render' should only send down information that that player has the
+    // right to see.
+    color: Color 
+  ) {
   }
 }
 
@@ -29,3 +34,28 @@ app.listen(port, () => {
     console.log(`set -x PORT 3001`);
   }
 });
+
+// This thing is going to immediately return a promise that will get fulfilled
+// later by something else.
+app.get('/poll', async (req, res) => {
+  const id = req.query.id;
+  const color: Player = req.query.color;
+  let session = lookup(id, color);
+  if (session === undefined) {
+    session = new Session();
+  }
+  let pending_promise = session.p;
+  if (pending_promise === undefined) {
+    pending_promise = new Promise<Any>();
+  }
+
+});
+
+class Session {
+  // Fulfil this promise to push something down. The client will have to open a
+  // new connection to get the next message.
+  // doc: https://medium.com/jspoint/typescript-promises-and-async-await-b842b55ee3fd
+  p: Promise<Any> 
+}
+
+
