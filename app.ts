173b363e7be4b0bd600bf127a4a7b39eda0eb3f7
Anukool Junnarkar <anukool.j@gmail.com>
2022-03-29 22:39:53 -0700

    [eod] push + maintain exactly 1 connection
    Added types for the Promise pool I want to use for push capability

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-type Player = 'red' | 'blue';
+type Color = 'red' | 'blue';
 
 app.get('/', (req, res) => {
   res.send('Hello Waterworld!');
@@ -18,10 +18,11 @@ class Board {
     // right to see.
     color: Color 
   ) {
+    // TODO
   }
 }
 
-// TODO: build client-side polling for push functionality. This will also be
+// TASK: build client-side polling for push functionality. This will also be
 // used to ensure that there's only one connection for a <game,player> pair at
 // a time.
 
@@ -37,25 +38,36 @@ app.listen(port, () => {
 
 // This thing is going to immediately return a promise that will get fulfilled
 // later by something else.
-app.get('/poll', async (req, res) => {
+app.get('/poll', (req, res): Promise<any> => {
   const id = req.query.id;
-  const color: Player = req.query.color;
+  const color: Color = req.query.color;
   let session = lookup(id, color);
   if (session === undefined) {
     session = new Session();
   }
   let pending_promise = session.p;
   if (pending_promise === undefined) {
-    pending_promise = new Promise<Any>();
+    pending_promise = new Promise<any>((resolve, reject) => {
+      // TODO: implement continuations here
+      }
+    );
   }
-
+  return pending_promise;
 });
 
 class Session {
   // Fulfil this promise to push something down. The client will have to open a
   // new connection to get the next message.
-  // doc: https://medium.com/jspoint/typescript-promises-and-async-await-b842b55ee3fd
-  p: Promise<Any> 
+  p: Promise<any> 
 }
 
+let sessions = []
 
+function lookup(id: string, color: Color): Session {
+  return null // TODO
+}
+
+// docs: 
+// async/await https://medium.com/jspoint/typescript-promises-and-async-await-b842b55ee3fd
+// typescript https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
+// classes https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
