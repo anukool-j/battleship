451f5c8828a9b07cbe5ff41d9c05ba1f2722dffd
Anukool Junnarkar <anukool.j@gmail.com>
2022-03-31 09:55:31 -0700

    API skeleton created (init, place, bomb, poll)
    * code somewhat reorganized for clarity
    * thought through session/board data model

--- app.ts
+++ app.ts
@@ -1,71 +1,107 @@
 const express = require('express')
 const app = express()
 
-type Color = 'red' | 'blue';
+app.use(express.static('static'));
+
+let port = process.env.PORT;
+app.listen(port, () => {
+  if (port === undefined) {
+    port = '3001';
+  }
+  console.log(`Battleship Classic listening on port ${port} ('set -x PORT <port>' to customize)`); 
+});
 
 app.get('/', (req, res) => {
   res.send('Hello Waterworld!');
 });
 
-app.use(express.static('static'));
 
 // Battleship Classic
 
+// TASK: build client-side polling for push functionality. This will also be
+// used to ensure that there's only one connection for a <board,color> pair at
+// a time.
+
+class Session {
+  readonly session_id: string;
+  board: Board = null;
+  color: Color = null;
+  
+  // Fulfill this promise to push something down. The client will have to open a
+  // new connection to get the next message.
+  pending_resolve_fn = null;
+
+  constructor(session_id) {
+    this.session_id = session_id;
+  }
+}
+
 class Board {
+  turn: Color = null; // null => waiting for placement
+
+  red_placements = []
+  blue_placements = []
+
+  red_bombs = []
+  blue_bombs = []
+
   render(
     // 'red' and 'blue' information is all stored in the same Board, but
     // 'render' should only send down information that that player has the
     // right to see.
     color: Color 
   ) {
-    // TODO
+    return `render-${color}`; // TODO
   }
 }
 
-// TASK: build client-side polling for push functionality. This will also be
-// used to ensure that there's only one connection for a <game,player> pair at
-// a time.
+type Color = 'red' | 'blue';
 
-// NOTE: wrap comments with 'gq<motion>'
 
-const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`Battleship Classic listening on port ${port}`); 
-  if (port === undefined) {
-    console.log(`set -x PORT 3001`);
-  }
+app.get('/init', (req, res) => {
+  const board_id = req.query.board_id; // null => create new board
+  const color = req.query.color; // null => assign automatically
+});
+
+app.get('/place', (req, res) => {
+});
+
+app.get('/bomb', (req, res) => {
 });
 
 // This thing is going to immediately return a promise that will get fulfilled
-// later by something else.
+// later by something else. 
 app.get('/poll', (req, res): Promise<any> => {
-  const id = req.query.id;
-  const color: Color = req.query.color;
-  let session = lookup(id, color);
+  const session_id = req.query.session_id;
+
+  let session = lookup(session_id);
   if (session === undefined) {
-    session = new Session();
+    // error out
+    return;
   }
-  let pending_promise = session.p;
-  if (pending_promise === undefined) {
-    pending_promise = new Promise<any>((resolve, reject) => {
-      // TODO: implement continuations here
-      }
-    );
-  }
-  return pending_promise;
+
+  if (session.pending_resolve_fn !== null) {
+    // this should be null -- error out
+    return;
+  }   
+
+  let promise = new Promise((resolve, reject) => {
+    // we have to stash this away
+    session.pending_resolve_fn = resolve;
+  });
+  
+  return promise;
 });
 
-class Session {
-  // Fulfil this promise to push something down. The client will have to open a
-  // new connection to get the next message.
-  p: Promise<any> 
-}
 
 let sessions = []
 
-function lookup(id: string, color: Color): Session {
-  return null // TODO
+function lookup(session_id): Session {
+  return sessions.find(x => session_id === x.board.session_id);
 }
+
+
+// TIP: wrap comments with 'gq<motion>'
 
 // docs: 
 // async/await https://medium.com/jspoint/typescript-promises-and-async-await-b842b55ee3fd
